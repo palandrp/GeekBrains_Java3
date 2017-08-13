2fab8be2004ec9bb03221db544b7e47f6746058d
palandr <palandrp@gmail.com>
2017-08-13 21:15:57 +0500

    Приложуха научилась добавлять сразу 10000 строк в таблицу.

--- src/main/java/ru/geekbrains/java3/dz/dz2/petrikovskiypavel/Main.java
+++ src/main/java/ru/geekbrains/java3/dz/dz2/petrikovskiypavel/Main.java
@@ -30,7 +30,9 @@ class DBUse {
             connection.setAutoCommit(false);
             create(connection,dbName,dbTable,dbCreateString);
             clear(connection,dbTable);
-            add(connection,addString,"krossy",100);
+            for (int i=1; i < 10001; i++) {
+                add(connection,addString,"good" + i,i*10);
+            }
             connection.commit();
         } catch (SQLException | NullPointerException exception) {
             exception.printStackTrace();
